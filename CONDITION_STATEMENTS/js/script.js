e163b6c300e8c04561b197dac9cda5e46754bc46
ui-dev-vignesh <vigneshperiyasamy.ui@gamil.com>
2023-12-21 19:00:31 +0530

    4 th commit

--- CONDITION_STATEMENTS/js/script.js
+++ CONDITION_STATEMENTS/js/script.js
@@ -12,6 +12,8 @@
 //     console.log("given number is equal to zero");
 // }
 
+// console.log("--------------------------------------");
+
 // nested if
 
 let english = 95, tamil = 45, malayalam = 45;
@@ -45,3 +47,129 @@ else {
     console.log("Result : Fail");
     console.log("Grade  : No Grade");
 }
+
+console.log("--------------------------------------");
+
+// switch case
+
+let num = 2;
+
+switch (num) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    default:
+        console.log("Not a number!");
+        break;
+}
+
+console.log("--------------------------------------");
+
+// combining case switch
+
+let letter = 'p';
+
+switch (letter) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        console.log(letter + " is a vowel");
+        break;
+    default:
+        console.log(letter + " is not a vowel!");
+        break;
+}
+
+console.log("--------------------------------------");
+
+// while
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+console.log("--------------------------------------");
+
+// do while
+
+let table = 3; limit = 10; j = 1;
+
+do {
+    console.log(table + " X " + j + " = " + (table * j));
+    j++;
+}
+while (j <= limit);
+
+console.log("--------------------------------------");
+
+// for
+
+let arr = [];
+for (let k = 0; k < 50; k++) {
+    arr.push(k);
+}
+console.log(arr);
+
+console.log("--------------------------------------");
+
+// nesteded for loop
+
+let nums = [];
+
+for (let I = 0; I < 5; I++) {
+    nums.push([]);
+    for (let J = 0; J < 5; J++) {
+        nums[I].push(J);
+    }
+}
+console.table(nums);
+
+console.log("--------------------------------------");
+
+// for of loop
+
+let names = ['ajay', 'krish', 'vasanth', 'vignesh', 'dinesh', 'boobesh', 'vinith'];
+
+for (let name of names) {
+    console.log(name);
+}
+
+console.log("--------------------------------------");
+
+// for in loop
+
+let user = {
+    username: 'vignesh',
+    dep: 'bca',
+    age: 23,
+    city: 'trichy',
+    contact: 9677579718
+}
+
+for (let prop in user) {
+    console.log(prop + " : " + user[prop]);
+}
+
+console.log("--------------------------------------");
---
+++ FUNCTION/js/script.js
@@ -0,0 +1,8 @@
+function multifly(num1, num2) {
+    let result = num1 * num2;
+    console.log("result : " + result);
+}
+
+multifly(4, 5);
+multifly(3, 20);
+multifly(8, 2);
